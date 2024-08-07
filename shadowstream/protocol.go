9cc23bcdb513647c7b94b38963020412c8d86468
wwqgtxx <wwqgtxx@gmail.com>
2024-08-07 15:49:23 +0800

    Fix parameter order in ChaCha20 constructor

--- shadowstream/protocol.go
+++ shadowstream/protocol.go
@@ -113,8 +113,12 @@ func New(method string, key []byte, password string) (shadowsocks.Method, error)
 	case "chacha20":
 		m.keyLength = chacha.KeySize
 		m.saltLength = chacha.NonceSize
-		m.encryptConstructor = chacha.NewChaCha20
-		m.decryptConstructor = chacha.NewChaCha20
+		m.encryptConstructor = func(key []byte, salt []byte) (cipher.Stream, error) {
+			return chacha.NewChaCha20(salt, key)
+		}
+		m.decryptConstructor = func(key []byte, salt []byte) (cipher.Stream, error) {
+			return chacha.NewChaCha20(salt, key)
+		}
 	default:
 		return nil, os.ErrInvalid
 	}
